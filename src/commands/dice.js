61f7e88b9d2985c1c03f63e0beea8022ffc072cd
Harton <hartontw@gmail.com>
2019-03-03 21:07:07 +0100

    Dice command improvement

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,14 +1,12 @@
 const Command = require('./command');
 const fs = require('fs');
+const package = require('../../package.json');
+const { RichEmbed } = require('discord.js');
 
-const images = {
-    d4: [],
-    d6: [],
-    d8: [],
-    d10: [],
-    d12: [],
-    d20: []
-}
+const githubUser = package.repository.url.match(/git\+https:\/\/github.com\/(.*?)\//)[1];
+const githubName = package.name;
+
+const url = `https://raw.githubusercontent.com/${githubUser}/${githubName}/master/src/assets/dice/d`;
 
 class Dice extends Command {
     static get description() {
@@ -32,27 +30,25 @@ class Dice extends Command {
             } else dices[i] = { amount: 1, type: Number(dice) };
         }
 
-        const fields = [];
+        const response = [];
+
         for (let i = 0; i < dices.length; i++) {
-            const name = `${dices[i].amount} d${dices[i].type}`;
-            let value = '';
-            for (let j = 0; j < dices[i].amount; j++) {
-                const rnd = Math.floor(Math.random() * dices[i].type) + 1;
-                const url = images[`d${dices[i].type}`][rnd];
-                value += `![](url)`;
-            }
+            const amount = dices[i].amount;
+            const type = dices[i].type;
 
-            fields.push({ name, value });
-        }
+            for (let j = 0; j < amount; j++) {
+                const value = Math.floor(Math.random() * type) + 1;
+                const imageUrl = url + type + '/' + value + '.png';
 
-        const response = {
-            embed: {
-                color: 3447003,
-                fields,
+                const embed = new RichEmbed()
+                    .setColor(0x00AE86)
+                    .setThumbnail(imageUrl);
+
+                response.push(await this.send(embed));
             }
         }
 
-        return await this.send(response);
+        return response;
     }
 }
 
